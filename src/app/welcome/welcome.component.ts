1093d7fe3b139c6fb8980fdb9b287964a1dac7fc
Acharya Anil Suryavanshi <thakurinfotech@gmail.com>
2020-05-25 10:52:34 +0530

    Animation typing effect

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -8,8 +8,39 @@ import { Component, OnInit } from '@angular/core';
 export class WelcomeComponent implements OnInit {
 
   constructor() { }
-
+  skill = ['Anil Kumar','Software Engineer','Yoga Trainer','Social Activist'];
+  type;
   ngOnInit(): void {
+
+  let idx=0;
+  let n=0;
+  let up = true;
+
+  setInterval(()=>{
+    
+      var word = this.skill[idx];
+      var ln = word.length;
+  
+      if(up){
+        this.type = word.slice(0,n);
+        n++
+      };
+      if(n===ln+1){up=false};
+      if(!up){
+       this.type = word.slice(0,n);
+        n--
+      }
+      if(n===0){
+        up=true
+        idx++
+      }
+      if(idx==this.skill.length){
+        idx=0;
+      }
+      
+  
+  },200)
+
   }
 
 }
